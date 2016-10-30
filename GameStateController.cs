373c55ccd1c6bd4b86e8143c8e658dfbc57afb04
Brayden Anderson <brayden.anderson@programmer.net>
2016-10-31 02:10:07 +1300

    GameStateController.cs
    The main game state controller (still needs a lot done to it before it can be committed to the master branch)

---
+++ GameStateController.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using System.Collections;
+
+class MainGameController : UnityEngine {
+  bool isPaused = False; // Controls whether the game is paused or not
+  /* Pause menu's, etc. can be figured out later, probably in a different class */
+
+}
